c1a181df0ceabb274fd82d740192d35310fe9bb4
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2023-05-10 03:43:04 +0000

    test

--- test.R
+++ test.R
@@ -9,14 +9,14 @@ model_output <- run_model()
 expected_mean <- 10
 model_mean  <- mean(model_output)
 mean_abs_error <- abs(model_mean - expected_mean)
-if (mean_abs_error > 0.2) {
-    stop('Sample mean has error ', mean_abs_error)
-}
+
+testthat::expect(mean_abs_error < 0.5, paste0('Sample mean has error ', mean_abs_error))
+
 
 # Is the output standard deviation close to the expected standard deviation?
 expected_sd <- 1
 model_sd <- sd(model_output)
 sd_abs_error <- abs(model_sd - expected_sd)
-if (sd_abs_error > 0.15) {
-    stop('Sample sd has error ', sd_abs_error)
-}
+
+
+testthat::expect(sd_abs_error < 0.25, paste0('Sample sd has error ', sd_abs_error))
